47847c8cd8ef2103cc35a75d0555cf90c4fc712c
takaaki.iida <takiida0605@gmail.com>
2019-02-04 16:28:25 +0900

    first commit

---
+++ src/main/kotlin/com/ninja/algorithmsanddatastructures/AlgorithmsAndDataStructuresApplication.kt
@@ -0,0 +1,12 @@
+package com.ninja.algorithmsanddatastructures
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgorithmsAndDataStructuresApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlgorithmsAndDataStructuresApplication>(*args)
+}
+
