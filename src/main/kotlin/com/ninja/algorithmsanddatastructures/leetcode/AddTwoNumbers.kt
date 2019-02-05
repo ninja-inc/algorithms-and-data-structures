7257d08ca80fb053d2e5530c95ccbcfe9b359072
takaaki.iida <takiida0605@gmail.com>
2019-02-05 18:49:20 +0900

    add two numbers

---
+++ src/main/kotlin/com/ninja/algorithmsanddatastructures/leetcode/AddTwoNumbers.kt
@@ -0,0 +1,77 @@
+package com.ninja.algorithmsanddatastructures.leetcode
+
+/**
+ * https://leetcode.com/problems/add-two-numbers/
+ */
+class AddTwoNumbers {
+    var isUp = false
+
+    fun run(l1: ListNode, l2: ListNode): ListNode {
+        var tmp = l1.value + l2.value
+
+        if (isUp) {
+            tmp += 1
+        }
+
+        if (tmp > 9) {
+            isUp = true
+            tmp -= 10
+        } else {
+            isUp = false
+        }
+
+        if (l1.next != null) {
+            return ListNode(tmp).also { it.next = this.run(l1.next!!, l2.next!!) }
+        } else
+            return ListNode(tmp)
+    }
+
+    // without recursion
+    fun run2(l1: ListNode, l2: ListNode): ListNode {
+        var ans = ListNode(0)
+        var tmpAns = ans
+
+        var tmp1 = l1
+        var tmp2 = l2
+
+        while (true) {
+            var tmp = tmp1.value + tmp2.value
+
+            if (isUp) {
+                tmp += 1
+            }
+
+            if (tmp > 9) {
+                isUp = true
+                tmp -= 10
+            } else {
+                isUp = false
+            }
+
+            tmpAns.next = ListNode(tmp)
+            tmpAns = tmpAns.next!!
+
+            if (tmp1.next == null) {
+                break
+            } else {
+                tmp1 = tmp1.next!!
+                tmp2 = tmp2.next!!
+            }
+        }
+
+        return ans.next!!
+    }
+}
+
+class ListNode {
+    val value: Int
+    var next: ListNode? = null
+
+    constructor(value: Int) {
+        this.value = value
+    }
+
+    fun linkNext(next: ListNode) {
+        this.next = next
+    }
+}
---
+++ src/test/kotlin/com/ninja/algorithmsanddatastructures/leetcode/AddTwoNumbersTest.kt
@@ -0,0 +1,36 @@
+package com.ninja.algorithmsanddatastructures.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class AddTwoNumbersTest {
+    private val addTwoNumbers = AddTwoNumbers()
+
+    @Test
+    fun test() {
+        val node2 = ListNode(2)
+        val node4 = ListNode(4)
+        val node3 = ListNode(3)
+
+        val node5 = ListNode(5)
+        val node6 = ListNode(6)
+        //val node4 = ListNode(4)
+
+        node2.linkNext(node4)
+        node4.linkNext(node3)
+
+        node5.linkNext(node6)
+        node6.linkNext(node4)
+
+
+        val result = addTwoNumbers.run2(node2, node5)
+
+        assertAll(
+                { assertThat(result.value).isEqualTo(7) },
+                { assertThat(result.next!!.value).isEqualTo(0) },
+                { assertThat(result.next!!.next!!.value).isEqualTo(8) },
+                { assertThat(result.next!!.next!!.next).isNull() }
+        )
+    }
+}
